26a4403125e661b1d01c620398a425a22c198847
Tal Endelman <tal.endelman@tufin.com>
2022-09-18 19:57:44 +0300

    New Class

---
+++ apps/robot/functions.py
@@ -0,0 +1,18 @@
+import asyncio
+
+from temi import Temi
+
+temi_ip_address = "172.20.10.7"
+# t = Temi('ws://172.20.10.7:8175')
+# await t.connect()
+
+async def connect_temi():
+    temi = Temi('ws://172.20.10.7:8175')
+    await temi.connect()
+    message = await temi.interface(url="https://www.baidu.com/").speak(sentence="HI TEMI").run()
+    print(message)
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(connect_temi())
+
+
+
