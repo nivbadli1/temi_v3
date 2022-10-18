5c09379963e2b7294379eb22d68f7f4907a32d28
Tal Endelman <tal.endelman@tufin.com>
2022-10-19 00:19:44 +0300

    start fixing lipaz notes

--- apps/robot/functions.py
+++ apps/robot/functions.py
@@ -9,7 +9,7 @@ temi_ip_address = "172.20.10.7"
 async def connect_temi():
     temi = Temi('ws://172.20.10.7:8175')
     await temi.connect()
-    message = await temi.interface(url="https://meet.google.com/dsf-ciew-iyo").speak(sentence="Going to do a call").run()
+    message = await temi.interface(url="https://meet.google.com/dsf-ciew-iyo").speak(sentence="Going to do a call").goto(location='spot1').run()
     print(message)
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(connect_temi())
